aa93e7992c4d0aeee61e164884b51ad1e7ac2a91
Alex dos Santos Moura <asmoura@sefaz.se.gov.br>
2020-06-22 07:42:41 -0300

    Adição de components (start e research-field) e models (repository e owner).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResearchFieldComponent } from './components/research-field/research-field.component';
+import { StartComponent } from './components/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResearchFieldComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/research-field/research-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research-field',
+  templateUrl: './research-field.component.html',
+  styleUrls: ['./research-field.component.css']
+})
+export class ResearchFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
